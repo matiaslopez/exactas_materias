509c81a452de168376c6be6a6275fe498c437815
Matías <matiaslopez@gmail.com>
2022-10-21 10:53:45 -0300

    Va

---
+++ jquery/table-sortable-master/dist/data_sp_local.js
@@ -0,0 +1,86 @@
+var data = [{
+	"Asignatura": "Matemática II (BIOL190004) ",
+	"Concepto": "Teórica",
+	"Turno": "Turno 1",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,9,0,0)",
+	"Hasta": "Date(1899,11,30,11,0,0)",
+	"Aula": 1101
+}, {
+	"Asignatura": "Métodos Numéricos (COMP930012) ",
+	"Concepto": "Teórico-Práctica",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,17,0,0)",
+	"Hasta": "Date(1899,11,30,22,0,0)",
+	"Aula": 1101
+}, {
+	"Asignatura": "Congreso - Durán/Marenco (11/15) (CLAIO 2022)(9-11/14-16)",
+	"Día": "Lunes",
+	"Fecha": "12/12",
+	"Desde": "Date(1899,11,30,9,0,0)",
+	"Hasta": "Date(1899,11,30,19,0,0)",
+	"Aula": 1101
+}, {
+	"Asignatura": "Ecuaciones Diferenciales A (MATE820012)/Ecuaciones Diferenciales B (MATE820013)",
+	"Concepto": "Teórico-Práctica",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,9,0,0)",
+	"Hasta": "Date(1899,11,30,14,0,0)",
+	"Aula": 1102
+}, {
+	"Asignatura": "Estadística para el Sistema Climático 1 (no usan 7/11-14/11)",
+	"Concepto": "Teórico-Práctica",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,14,0,0)",
+	"Hasta": "Date(1899,11,30,19,0,0)",
+	"Aula": 1102
+}, {
+	"Asignatura": "Matemática II (BIOL190004)",
+	"Concepto": "Laboratorio",
+	"Turno": "Turno 1-1",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,11,0,0)",
+	"Hasta": "Date(1899,11,30,14,0,0)",
+	"Aula": 1103
+}, {
+	"Asignatura": "Oceanografía Satelital",
+	"Día": "Lunes",
+	"Fecha": "14/11-21/11",
+	"Desde": "Date(1899,11,30,14,0,0)",
+	"Hasta": "Date(1899,11,30,18,0,0)",
+	"Aula": 1103
+}, {
+	"Asignatura": "Informática Educativa (PROF930005)",
+	"Concepto": "Teórico-Práctica",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,18,0,0)",
+	"Hasta": "Date(1899,11,30,22,0,0)",
+	"Aula": 1103
+}, {
+	"Asignatura": "Matemática II (BIOL190004)",
+	"Concepto": "laboratorio",
+	"Turno": "Turno 1-2",
+	"Día": "Lunes",
+	"Desde": "Date(1899,11,30,11,0,0)",
+	"Hasta": "Date(1899,11,30,14,0,0)",
+	"Aula": 1104
+}, {
+	"Asignatura": "Estadística para el Sistema Climático 1 (ATMO180009) (7/11-14/11)",
+	"Concepto": "Teórico-Práctica",
+	"Día": "Lunes",
+	"Fecha": "7/11-14/11",
+	"Desde": "Date(1899,11,30,14,0,0)",
+	"Hasta": "Date(1899,11,30,19,0,0)",
+	"Aula": 1104
+}]
+
+var columns = {
+    'Asignatura': 'Asignatura',
+    'Concepto': 'Concepto',
+    'Turno': 'Turno',
+    'Fecha': 'Fecha',
+    'Día': 'Día',
+    'Desde': 'Desde',
+    'Hasta': 'Hasta',
+    'Aula': 'Aula'
+}
